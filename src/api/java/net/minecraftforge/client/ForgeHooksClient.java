0a07941874cdd704b6f467a7980a8b37f95973db
Matthew Messinger <mattmess1221@gmail.com>
2017-03-28 02:02:23 -0400

    Fix modded armors. Bug 1: forge postinit wasn't being called. Probably issue with event stub. Got rid of it to avoid the hassle. Bug 2: ForgeHooksClient stub had bad signatures.

--- src/api/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/api/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -1,17 +1,19 @@
 package net.minecraftforge.client;
 
-import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 
 // stub
 public class ForgeHooksClient {
 
-    public static String getArmorTexture(EntityLivingBase entity, ItemStack armor, String def, int slot, String type) {
+    public static String getArmorTexture(Entity entity, ItemStack armor, String def, EntityEquipmentSlot slot, String type) {
         return def;
     }
 
-    public static ModelBase getArmorModel(EntityLivingBase entity, ItemStack item, int slot, ModelBase def) {
+    public static ModelBiped getArmorModel(EntityLivingBase entity, ItemStack item, EntityEquipmentSlot slot, ModelBiped def) {
         return def;
     }
 
