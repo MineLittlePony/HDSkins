2959882447d0a6bdaed78be0ffef3b967b916a07
Sollace <sollacea@gmail.com>
2019-05-25 18:36:39 +0200

    Update and implement all the stuff that interacts with the loader

---
+++ src/main/java/com/minelittlepony/hdskins/FabMod.java
@@ -0,0 +1,34 @@
+package com.minelittlepony.hdskins;
+
+import java.nio.file.Path;
+import java.util.function.Function;
+
+import com.minelittlepony.hdskins.mixin.MixinEntityRenderDispatcher;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.entity.Entity;
+
+public class FabMod implements ClientModInitializer, IModUtilities {
+
+    private final HDSkins hdskins = new HDSkins(this, FabricLoader.getInstance().getConfigDirectory().toPath());
+
+    @Override
+    public void onInitializeClient() {
+        hdskins.postinit();
+    }
+
+    @Override
+    public <T extends Entity> void addRenderer(Class<T> type, Function<EntityRenderDispatcher, EntityRenderer<T>> renderer) {
+        EntityRenderDispatcher mx = MinecraftClient.getInstance().getEntityRenderManager();
+        ((MixinEntityRenderDispatcher)mx).getRenderers().put(type, renderer.apply(mx));
+    }
+
+    @Override
+    public Path getAssetsDirectory() {
+        return FabricLoader.getInstance().getGameDirectory().toPath().resolve("assets");
+    }
+}
