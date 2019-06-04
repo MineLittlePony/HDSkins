7b48e3f22d34c2a2d5abdd3f438c4026f003f766
Matthew Messinger <mattmess1221@gmail.com>
2019-06-03 21:39:50 -0400

    Move title mixin to kirin

--- src/main/java/com/minelittlepony/hdskins/FabMod.java
+++ src/main/java/com/minelittlepony/hdskins/FabMod.java
@@ -4,11 +4,18 @@ import javax.annotation.Nullable;
 
 import com.minelittlepony.common.client.IModUtilities;
 
+import com.minelittlepony.common.client.gui.element.Button;
+import com.minelittlepony.common.client.gui.style.Style;
+import com.minelittlepony.common.event.ScreenInitCallback;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
-public class FabMod implements ClientModInitializer, ClientTickCallback, IModUtilities {
+public class FabMod implements ClientModInitializer, IModUtilities {
 
     @Nullable
     private HDSkins hd;
@@ -17,13 +24,13 @@ public class FabMod implements ClientModInitializer, ClientTickCallback, IModUti
 
     @Override
     public void onInitializeClient() {
-        ClientTickCallback.EVENT.register(this);
+        ClientTickCallback.EVENT.register(this::onClientTick);
+        ScreenInitCallback.EVENT.register(this::onScreenInit);
 
         hd = new HDSkins(this);
     }
 
-    @Override
-    public void tick(MinecraftClient client) {
+    private void onClientTick(MinecraftClient client) {
         if (hd == null) {
             return;
         }
@@ -32,6 +39,14 @@ public class FabMod implements ClientModInitializer, ClientTickCallback, IModUti
             firstTick = false;
 
             hd.postInit(client);
+        }
+    }
+
+    private void onScreenInit(Screen screen, ScreenInitCallback.ButtonList buttons) {
+        if (screen instanceof TitleScreen) {
+            buttons.add(new Button(screen.width - 50, screen.height - 50, 20, 20).onClick(sender -> {
+                MinecraftClient.getInstance().openScreen(HDSkins.getInstance().createSkinsGui());
+            }).setStyle(new Style().setIcon(new ItemStack(Items.LEATHER_LEGGINGS), 0x3c5dcb)));
         }
     }
 }
