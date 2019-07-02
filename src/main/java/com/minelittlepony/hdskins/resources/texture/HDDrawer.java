570db682736e6c91afc05947b09bac6d0ced2e3f
Sollace <sollacea@gmail.com>
2019-07-02 11:44:11 +0200

    Change these names to properly reflect mojang's parameters

--- src/main/java/com/minelittlepony/hdskins/resources/texture/HDDrawer.java
+++ src/main/java/com/minelittlepony/hdskins/resources/texture/HDDrawer.java
@@ -5,10 +5,10 @@ import com.minelittlepony.common.util.TextureConverter;
 public interface HDDrawer extends TextureConverter.Drawer {
 
     @Override
-    default void copy(int xFrom, int yFrom, int xTo, int yTo, int width, int height, boolean mirrorX, boolean mirrorY) {
+    default void copy(int xFrom, int yFrom, int xOffset, int yOffset, int width, int height, boolean mirrorX, boolean mirrorY) {
         int scale = getImage().getWidth() / 64;
         getImage().method_4304(
-                xFrom * scale, yFrom * scale, xTo * scale, yTo * scale,
+                xFrom * scale, yFrom * scale, xOffset * scale, yOffset * scale,
                 width * scale, height * scale, mirrorX, mirrorY);
     }
 }
