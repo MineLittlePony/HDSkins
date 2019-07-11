3316a25f93fb096c4b3ff2c34e96c7ddadbc2b31
Sollace <sollacea@gmail.com>
2019-07-11 19:14:33 +0200

    Update kirin

--- src/main/java/com/minelittlepony/hdskins/HDConfig.java
+++ src/main/java/com/minelittlepony/hdskins/HDConfig.java
@@ -1,10 +1,12 @@
 package com.minelittlepony.hdskins;
 
 import com.minelittlepony.common.util.settings.JsonConfig;
+import com.minelittlepony.common.util.settings.Setting;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class HDConfig extends JsonConfig {
 
-    public final Setting<Path> lastChosenFile = new Value<>("lastChosenFile", Paths.get("/"));
+    public final Setting<Path> lastChosenFile = value("lastChosenFile", Paths.get("/"));
 }
