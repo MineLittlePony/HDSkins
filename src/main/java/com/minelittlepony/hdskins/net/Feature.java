8c5f905375b54c009a1d37c83bd913f5ed5a9b96
Sollace <sollacea@gmail.com>
2019-08-24 00:26:05 +0200

    Clean up some more of IDEA's mess

--- src/main/java/com/minelittlepony/hdskins/net/Feature.java
+++ src/main/java/com/minelittlepony/hdskins/net/Feature.java
@@ -5,45 +5,45 @@ package com.minelittlepony.hdskins.net;
  */
 public enum Feature {
     /**
-     * Whether a net has write access.
-     * i.e. If the net allows for users to upload a new skin.
+     * Whether a server has write access.
+     * i.e. If the server allows for users to upload a new skin.
      */
     UPLOAD_USER_SKIN,
     /**
-     * Whether a net allows for downloading and saving a user's skin.
+     * Whether a server allows for downloading and saving a user's skin.
      * Most servers should support this.
      */
     DOWNLOAD_USER_SKIN,
     /**
-     * Whether a net has delete access.
-     * i.e. If the net allows a user to deleted a previously uploaded skin.
+     * Whether a server has delete access.
+     * i.e. If the server allows a user to deleted a previously uploaded skin.
      */
     DELETE_USER_SKIN,
     /**
-     * Whether a net can send a full list of skins for a given profile.
+     * Whether a server can send a full list of skins for a given profile.
      * Typically used for servers that keep a record of past uploads
      * and/or allow for switching between past skins.
      */
     FETCH_SKIN_LIST,
     /**
-     * Whether a net supports thin (Alex) skins or just default (Steve) skins.
+     * Whether a server supports thin (Alex) skins or just default (Steve) skins.
      * Servers without this will typically fall back to using the player's uuid on the client side.
      *
      * (unused)
      */
     MODEL_VARIANTS,
     /**
-     * Whether a net allows for uploading alternative skin types. i.e. Cape, Elytra, Hats and wears.
+     * Whether a server allows for uploading alternative skin types. i.e. Cape, Elytra, Hats and wears.
      */
     MODEL_TYPES,
     /**
-     * Whether a net will accept arbitrary extra metadata values with skin uploads.
+     * Whether a server will accept arbitrary extra metadata values with skin uploads.
      *
      * (unused)
      */
     MODEL_METADATA,
     /**
-     * Whether a net can provide a link to view a user's profile online,
+     * Whether a server can provide a link to view a user's profile online,
      * typically through a web-portal.
      */
     LINK_PROFILE
