daa8070f2bb6c9bc093d3b54e49ace2482c0e1de
Sollace <sollacea@gmail.com>
2019-08-28 23:11:36 +0200

    Switch to FilenameUtils

--- src/main/java/com/minelittlepony/hdskins/util/net/FileTypes.java
+++ src/main/java/com/minelittlepony/hdskins/util/net/FileTypes.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.FilenameUtils;
+
 import com.google.common.base.Strings;
 
 public class FileTypes {
@@ -18,23 +20,11 @@ public class FileTypes {
     }
 
     public static String getExtension(Path path) {
-        String[] split = path.getFileName().toString().split("\\.");
-        return split.length > 1 ? split[split.length - 1] : "";
-    }
-
-    public static String removeExtension(Path path) {
-        String sPath = path.toString();
-        String ext = getExtension(path);
-
-        if (ext.isEmpty()) {
-            return sPath;
-        }
-        return sPath.substring(0, sPath.length() - ext.length() - 1);
+        return FilenameUtils.getExtension(path.getFileName().toString());
     }
 
     public static Path changeExtension(Path path, String extension) {
-
-        String noExt = removeExtension(path);
+        String noExt = FilenameUtils.removeExtension(path.toString());
 
         return Paths.get(noExt + extension);
     }
