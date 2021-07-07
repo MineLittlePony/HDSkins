654ed5747ac55b5fcab2d886c0650662ef3e7746
Sollace <sollacea@gmail.com>
2021-07-07 12:53:54 +0200

    Fixed incompatibility with Better Beds. Closes #44

--- src/main/java/com/minelittlepony/hdskins/client/dummy/DummyPlayerRenderer.java
+++ src/main/java/com/minelittlepony/hdskins/client/dummy/DummyPlayerRenderer.java
@@ -1,12 +1,18 @@
 package com.minelittlepony.hdskins.client.dummy;
 
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BedBlockEntity;
+import net.minecraft.block.enums.BedPart;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.recipebook.ClientRecipeBook;
 import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayers;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.BlockRenderManager;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
@@ -15,7 +21,10 @@ import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.stat.StatHandler;
 import net.minecraft.util.Lazy;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 import com.minelittlepony.common.client.gui.OutsideWorldRenderer;
 
@@ -50,11 +59,22 @@ public class DummyPlayerRenderer {
         }
     }
 
+    static void renderBlockState(World world, BlockState state, MatrixStack stack, VertexConsumerProvider renderContext) {
+        BlockRenderManager blockRenderer = MinecraftClient.getInstance().getBlockRenderManager();
+        blockRenderer.getModelRenderer().render(world, blockRenderer.getModel(state),
+                state, BlockPos.ORIGIN, stack,
+                renderContext.getBuffer(RenderLayers.getMovingBlockLayer(state)),
+                false, new Random(),
+                state.getRenderingSeed(BlockPos.ORIGIN), OverlayTexture.DEFAULT_UV);
+    }
+
     static class BedHead extends BedBlockEntity {
         public static BedHead instance = new BedHead();
 
         public BedHead() {
-            super(BlockPos.ORIGIN, Blocks.RED_BED.getDefaultState());
+            super(BlockPos.ORIGIN, Blocks.RED_BED.getDefaultState()
+                    .with(BedBlock.PART, BedPart.HEAD)
+                    .with(BedBlock.FACING, Direction.SOUTH));
         }
 
         public void render(Entity entity, MatrixStack stack, VertexConsumerProvider renderContext) {
@@ -63,9 +83,18 @@ public class DummyPlayerRenderer {
             stack.scale(-1, 1, flipReality ? -1 : 1);
             stack.translate(-0.5, 0, 0);
 
-            OutsideWorldRenderer.configure(entity.getEntityWorld())
-                .get(this)
-                .render(this, 1, stack, renderContext, 0xF000F0, OverlayTexture.DEFAULT_UV);
+            World world = entity.getEntityWorld();
+
+            BlockEntityRenderer<BedHead> renderer = OutsideWorldRenderer.configure(world).get(this);
+
+            if (renderer != null) {
+                renderer.render(this, 1, stack, renderContext, 0xF000F0, OverlayTexture.DEFAULT_UV);
+            } else {
+                BlockState state = getCachedState();
+                renderBlockState(world, state, stack, renderContext);
+                stack.translate(0, 0, -1);
+                renderBlockState(world, state.with(BedBlock.PART, BedPart.FOOT), stack, renderContext);
+            }
 
             stack.pop();
         }
@@ -79,10 +108,11 @@ public class DummyPlayerRenderer {
         public void render(MatrixStack stack, VertexConsumerProvider renderContext) {
             stack.push();
 
-            @SuppressWarnings("unchecked")
-            EntityRenderer<BoatEntity> render = (EntityRenderer<BoatEntity>)MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(this);
+            EntityRenderer<? super MrBoaty> renderer = MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(this);
 
-            render.render(this, 0, 0, stack, renderContext, 0xF000F0);
+            if (renderer != null) {
+                renderer.render(this, 0, 0, stack, renderContext, 0xF000F0);
+            }
 
             stack.pop();
         }
