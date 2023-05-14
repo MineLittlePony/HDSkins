7c43a6b7699113b2b0a376c3897f693b6fee9d4e
Matthew Messinger <mattmess1221@gmail.com>
2023-05-14 17:20:13 -0400

    Re-add old methods in TexturePayload as deprecated

--- src/main/java/com/minelittlepony/hdskins/server/TexturePayload.java
+++ src/main/java/com/minelittlepony/hdskins/server/TexturePayload.java
@@ -20,6 +20,38 @@ public record TexturePayload(
         this(System.currentTimeMillis(), profile.getId(), profile.getName(), true, new HashMap<>(textures));
     }
 
+    /**
+     * @deprecated Use {@link #timestamp()} instead.
+     */
+    @Deprecated
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @deprecated Use {@link #profileId()} instead.
+     */
+    @Deprecated
+    public UUID getProfileId() {
+        return profileId;
+    }
+
+    /**
+     * @deprecated Use {@link #profileName()} instead.
+     */
+    @Deprecated
+    public String getProfileName() {
+        return profileName;
+    }
+
+    /**
+     * @deprecated Use {@link #textures()} instead.
+     */
+    @Deprecated
+    public Map<SkinType, MinecraftProfileTexture> getTextures() {
+        return textures;
+    }
+
     public Optional<MinecraftProfileTexture> getTexture(SkinType type) {
         return textures.containsKey(type) ? Optional.of(textures.get(type)) : Optional.empty();
     }
