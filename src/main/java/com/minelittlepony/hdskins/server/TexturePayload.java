30ddeef7f8030e29a9a10c9431d17bf49c199504
Matthew Messinger <mattmess1221@gmail.com>
2025-02-09 11:25:39 -0500

    Fix valhalla client implementation for api v1 (#81)
    Fixes #80

--- src/main/java/com/minelittlepony/hdskins/server/TexturePayload.java
+++ src/main/java/com/minelittlepony/hdskins/server/TexturePayload.java
@@ -2,6 +2,7 @@ package com.minelittlepony.hdskins.server;
 
 import java.util.*;
 
+import com.minelittlepony.hdskins.client.HDSkins;
 import com.minelittlepony.hdskins.profile.SkinType;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
@@ -52,5 +53,24 @@ public class TexturePayload {
 
     public Optional<MinecraftProfileTexture> getTexture(SkinType type) {
         return textures.containsKey(type) ? Optional.of(textures.get(type)) : Optional.empty();
+    }
+
+    public static class Textures extends HashMap<SkinType, MinecraftProfileTexture> {
+        private static final long serialVersionUID = 8314133197016994678L;
+
+        public Textures(Map<SkinType, MinecraftProfileTexture> textures) {
+            putAll(textures);
+        }
+
+        public Textures() { }
+
+        @Override
+        public MinecraftProfileTexture put(SkinType type, MinecraftProfileTexture texture) {
+            texture = super.put(type, texture);
+            if (texture != null) {
+                HDSkins.LOGGER.warn("Duplicate texture for skin type " + type);
+            }
+            return null;
+        }
     }
 }
