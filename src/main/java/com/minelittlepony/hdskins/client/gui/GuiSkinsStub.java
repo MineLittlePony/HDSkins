f359d94f5780a6212069de57ffbe1fb60b4eba0d
Sollace <sollacea@gmail.com>
2025-10-16 00:10:43 +0100

    Fix the stub screen not replacing itself properly

--- src/main/java/com/minelittlepony/hdskins/client/gui/GuiSkinsStub.java
+++ src/main/java/com/minelittlepony/hdskins/client/gui/GuiSkinsStub.java
@@ -1,7 +1,5 @@
 package com.minelittlepony.hdskins.client.gui;
 
-import java.util.concurrent.CompletableFuture;
-
 import com.minelittlepony.hdskins.HDSkinsServer;
 
 import net.minecraft.client.MinecraftClient;
@@ -29,10 +27,8 @@ public class GuiSkinsStub extends Screen {
     }
 
     @Override
-    public void onDisplayed() {
-        CompletableFuture.runAsync(() -> {
-            MinecraftClient.getInstance().setScreen(child);
-        }, MinecraftClient.getInstance());
+    public void tick() {
+        MinecraftClient.getInstance().setScreen(child);
     }
 
 }
